b46dba84b608720daa6798d1d6e934b3067d83db
Garth Martinsen <garth.martinsen@gmail.com>
2021-10-04 17:41:31 -0700

    Contains config info for the processor such as db_url db_interval, analog smooth num, digital smooth num, initial values at start up.

---
+++ config.py
@@ -0,0 +1,28 @@
+# file: config.py
+
+src = 'uno'
+
+input_names = ["A1", "D2", "D3"]  # what pins are going to be sampled for this app  # noqa: E501
+
+#  **************** Lookups ********************************************
+#  Lookup for number of samples to be averaged, key: A2D  value: num
+
+analog_num_lookup = {25: 100, 50: 75, 100: 25, 2048: 25}
+
+#  Lookup for arduino db_interval, key:A2D, value:seconds
+# function returns:  0-99:240   100-199:60  200-399:30  400-1023: 60
+db_interval_lookup = {30: 300, 100: 240, 200: 60, 400: 30, 500: 60, 2048: 240}
+
+#  **************** Values *********************************************
+
+initial_db_interval = 60         # seconds
+
+initial_analog_num = 10
+
+analog_3sigma = 0   # to remove this constraint, set it to 0
+
+digital_num = 5
+
+usb_port = '/dev/cu.usbmodem14301'
+
+db_url = 'http://127.0.0.1:8000/pins/'
