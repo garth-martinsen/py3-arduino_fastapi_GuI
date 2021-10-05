8cc77da17cefa3195830a74885cc6c4a35ab7b7e
Garth Martinsen <garth.martinsen@gmail.com>
2021-10-04 17:47:58 -0700

    This is the gui that will call sampler.get_data(). Currently it does not plot but later plot_gui.py will be inforporated into this. For now the GUI just displays a small view with values for: A1, D2, D3, ts (timestamp)

---
+++ arduino_gui.py
@@ -0,0 +1,42 @@
+#  file: junk_gui.py
+import PySimpleGUI as sg
+import sampler
+import pdb
+
+
+def start_sampler():
+    sampler.initialize()
+
+
+def get_data():
+    data = sampler.get_data()
+    if data is not None:
+        update_controls(data)
+
+
+def update_controls(data):
+    # Only show ts to the second
+    window["TS"].update(data["ts"][:-7])
+    window["A1"].update(data["A1"].value)
+    window["D2"].update(data["D2"].value)
+    window["D3"].update(data["D3"].value)
+
+
+controls = [
+    [sg.Text("D2"), sg.Input("0", size=(3, 1), key="D2"),
+     sg.Text("D3"), sg.Input("0", size=(3, 1), key="D3")],
+    [sg.Text("A1"), sg.Input("", size=(7, 1), key="A1")],
+    [sg.Text("TS"), sg.Input("", size=(25, 1), key="TS")]
+]
+
+window = sg.Window("ChickenCoop", controls, return_keyboard_events=True,
+                   size=(250, 80), finalize=True, keep_on_top=True)
+
+
+start_sampler()
+
+while True:
+    event, values = window.read(timeout=1000)  # every second to get data.
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+    get_data()
