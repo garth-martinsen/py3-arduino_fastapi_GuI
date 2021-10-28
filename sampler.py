64857b7709233948ff6ce637b924e8a47b721325
Garth Martinsen <garth.martinsen@gmail.com>
2021-10-28 16:51:14 -0700

    renamed objects, clarified comments. added timestampe in call to initialize the signal_processor. handled http response.

--- sampler.py
+++ sampler.py
@@ -1,15 +1,18 @@
 # file: sampler.py
 
-import config as c
+import config 
 import signal_processor as p
 from pyfirmata import Arduino, util
 import datetime
+import time
+import requests
 
-ap = p.ArduinoSignalProcessor()
+sigproc = p.ArduinoSignalProcessor()
 
 
 def initialize():
-    ap.initialize(c, Arduino, util)
+    sigproc.initialize(config, Arduino, util, time.time())
+    print(f'Started sampling at: {str(time.ctime())}')
 
 
 def get_data():
@@ -18,18 +21,21 @@ def get_data():
     many InputPins were specified in the config.py file. Each InputPin has 
     3 attributes: sig_type, pin, value.  examples: for pins:
     ....'A1': ('analog', 1 , 978)
-    ....'D2':('digital', 2, True).
+    ....'D2':('digital', 2, 1).  # bool is best modeled as 0|1 in this app
     The algorithm to determine if a signal has changed depends on 
     ....A2D noise for analogs sampling +/-3 sigma envelope
-    ....digital signal transition (True->False or False->True)
-    ....time from the last db save 
-    These values are set in the config.py file. 
+    ....digital signal transition (1->0 or 0->1)
+    ....time from the last db save in seconds
+    These values are specified in the config.py file. 
     This function is called upon a timeout in the GUI window.read(...).
     So the GUI is updated every time it calls processor.get_data() but the 
     db is updated only when it is called by the GUI AND 
     change criteria are met.
     '''
-    ap.collect_inputs()
-    if ap.changed():
-        ap.update_db()
-    return {**ap.current_state, "ts": str(datetime.datetime.now())}
+    sigproc.collect_inputs()
+    dts = datetime.datetime.now()
+    if sigproc.changed(dts.timestamp()):
+        response = sigproc.update_db(dts)
+        if response.ok:
+            print(f'OK: {response.json()}')
+    return {**sigproc.current_state, "ts": str(dts)}
